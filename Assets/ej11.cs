132ef787dc53c8b7a1d825b639184ff01352dfbb
nicolas-chab <47203227@est.ort.edu.ar>
2022-04-15 15:20:46 -0300

    ej10,11,12,13 hecho

---
+++ Assets/ej11.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej11 : MonoBehaviour
+{
+    public int año;
+    public int mes;
+    public int dia;
+    public string NombredelComprador;
+    public string Productosolicitado;
+     public int Cantidadsolicitada;
+    public int PrecioUnitario;
+    int TotalaPagar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TotalaPagar = PrecioUnitario * Cantidadsolicitada;
+        Debug.Log("Fecha de Compra: " + año + "/" + mes +"/"+ dia + " Nombre del Comprador: " + NombredelComprador + " Producto solicitado: " +Productosolicitado+" Cantidad solicitada: " +Cantidadsolicitada+ 
+            " Precio Unitario: $ " + PrecioUnitario + " Total a Pagar: $ " + TotalaPagar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ej13.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej13 : MonoBehaviour
+{
+    public int horas;
+    int paga;
+    int horasextra;
+    int pagaextra;
+    int pagatot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+      
+          
+       
+        if (horas > 40)
+        {
+            horasextra = horas - 40;
+            pagaextra = (horasextra * 20);
+            paga = 40 * 16;
+            pagatot = paga + pagaextra;
+            Debug.Log("cantidad de horas " + horas + " salario correspondiente:$ " + pagatot);
+        }
+        else
+        {
+            paga = horas * 16;
+            Debug.Log("cantidad de horas " + horas + " salario correspondiente " + paga);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
